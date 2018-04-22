f5cc35c76473675810a72a964d177ca48d7ace79
Glord <gnjemyc@gmail.com>
2018-04-23 00:41:58 +0800

    Add English Name and Release Time

--- WebCrawler.py
+++ WebCrawler.py
@@ -6,18 +6,19 @@ def get_thisWeek_newMovies():
 
     webSite = 'https://movies.yahoo.com.tw/movie_thisweek.html'
 
+    print('本周新片：')
     while webSite:
         resp = requests.get(webSite)
         soup = BeautifulSoup(resp.text, 'html.parser')
-        thisWeekMoviePage = soup.find_all('div','release_movie_name')
+        thisWeekMoviePage = soup.find_all('div','release_info_text')
         #print(thisWeekMoviePage)
-        
 
         for a in thisWeekMoviePage:
-            movieName = a.find('a')['data-ga']
-            index = movieName.rfind(',')
-            print('本周新片:' + movieName[index+1:-1])
-
+            movieName = a.find_all('a','gabtn')
+            movieReleaseTime = a.find('div','release_movie_time').text
+            movieInfo = a.find('span','jq_text_overflow_180 jq_text_overflow_href_list').text.strip()
+            print('片名： ' + movieName[0].text.strip() + ' - ' + movieName[1].text.strip() + ' || ' + movieReleaseTime + ' || 簡介：' + movieInfo)
+            
         nextPage = soup.find('li','nexttxt')
 
         try:
@@ -27,4 +28,4 @@ def get_thisWeek_newMovies():
             break 
     
 if __name__ == '__main__':
-    get_thisWeek_newMovies()
+    get_thisWeek_newMovies() 
