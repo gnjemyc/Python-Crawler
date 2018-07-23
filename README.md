ebbd352a382efe28cfd142c08029a704f7d8b842
Glord <gnjemyc@gmail.com>
2018-07-23 23:34:27 +0800

    Use Flask to be an API

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TestCode
 Begin to Python, practice it stpe by step
+Use BeautifulSoup,Flask,Slack API
--- WebCrawler.py
+++ WebCrawler.py
@@ -1,12 +1,24 @@
+from flask import Flask,jsonify,request
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+stores = [{
+    'name': 'Elton\'s first store',
+    'items': [{'name':'my item 1', 'price': 30 }],
+    },
+    {
+    'name': 'Elton\'s second store',
+    'items': [{'name':'my item 2', 'price': 15 }],
+    },
+]
 
-def get_thisWeek_newMovies():
-
+#get 
+@app.route('/movie')
+def get_movie():
     webSite = 'https://movies.yahoo.com.tw/movie_thisweek.html'
 
-    result = '本周新片：'
+    result = '本周新片：\n'
 
     while webSite:
         resp = requests.get(webSite)
@@ -18,9 +30,13 @@ def get_thisWeek_newMovies():
             movieName = a.find_all('a','gabtn')
             movieReleaseTime = a.find('div','release_movie_time').text
             movieInfo = a.find('span','jq_text_overflow_180 jq_text_overflow_href_list').text.strip()
-            moviePreview = a.find('a','btn_s_vedio')['href']
+            if(a.find('a','btn_s_vedio').get('href')):
+                moviePreview = a.find('a','btn_s_vedio').get('href')
+            else :
+                moviePreview = ''
+            result +='******************\n'
             result += ('片名： ' + movieName[0].text.strip() + ' - ' + movieName[1].text.strip() + ' || ' + movieReleaseTime + ' || 預告片：' + moviePreview + ' || 簡介：' + movieInfo + '\n\n')
-            
+
         nextPage = soup.find('li','nexttxt')
 
         try:
@@ -31,8 +47,48 @@ def get_thisWeek_newMovies():
 
     postdata = { "text":result }
 
-    r = requests.post("https://hooks.slack.com/services/TADVBAC69/BADP03L11/xxxxxxxxxxxxxxxxxxx",headers = {"content-type": "application/json"},json=postdata)
+    r = requests.post("https://hooks.slack.com/services/xxxxxxxxxxxxxxxx",headers = {"content-type": "application/json"},json=postdata)
     print(r)
     
-if __name__ == '__main__':
-    get_thisWeek_newMovies() 
+    return jsonify ([{'success': 'true','result':'ok'}])
+#get /store
+@app.route('/store')
+def get_stores():
+    return jsonify(stores)
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+        return jsonify ({'message': 'store not found'})
+#get /store/<name>/item data: {name :}
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify( {'items':store['items'] } )
+    return jsonify ({'message':'store not found'})
+#post /store data: {name :}
+@app.route('/store' , methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name':request_data['name'],
+        'items':[]
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+#post /store/<name> data: {name :}
+@app.route('/store/<string:name>/item' , methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+        store['items'].append(new_item)
+        return jsonify(new_item)
+
+app.run(port=5000, debug=True)
